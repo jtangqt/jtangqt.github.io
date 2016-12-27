97f1d66dc75206e40196546c43c13040d74a75a5
jtangqt <ajasmineflower@gmail.com>
2016-12-27 12:25:37 +0800

    new redesigned site

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// $(document).ready(function(){
+
+// 	$(window).on("scroll",function(){
+// 		//transitioning the header from unseen to seen   
+// 	    var height = $(window).height();
+
+// 	    if( window.scrollY < height ) {
+// 	      $(".overview").removeClass("sticky");
+// 	    } else { //window.scrollY >= 320
+// 	      $(".overview").addClass("sticky");
+// 	    }
+
+// 		if( window.scrollY < 930 ) {
+// 	    	$(".overview").css("background-color","rgba(255,255,255, 0.25)");
+// 	    }
+// 	    else {
+// 	    	$(".overview").css("background-color","rgba(255,255,255,0.5)");	
+// 	    }
+
+// 	});
+
+// }
